e073a5c18fb834235940de6ea22e3ffd1eff0880
Shivam Litoria <99090581+Litoriashiv@users.noreply.github.com>
2023-02-13 19:37:53 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,26 +1,23 @@
 #include "home.h"
 #include <fstream>
 #include <sstream>
+#include <string.h>
 #include <QApplication>
 
 CascadeClassifier face_cascade;
-string cf;
-string ff;
-string eigenFile;
-string studentFile;
-string attendanceFile;
-string name;
-string videoPath;
-string xmlPath = "/home/teja/OpenCV/opencv/data/haarcascades/";
+string faceFolder;
+string xmlPath = "/home/teja/OpenCV/opencv/data/haarcascades/haarcascade_frontalface_alt.xml";
 
-int attendance = 0;
+AddStudent::AddStudent(QString classFolder, QSpinBox *rollEdit)
+{
+    filenumber = 10;
+    this->cf = classFolder.toStdString();
+    this->ff = (classFolder + "faces/").toStdString();
+    this->ef = (classFolder + "eigenface.yml").toStdString();
+    this->roll = rollEdit->text().toStdString();
+}
 
-int filenumber = 10;
-
-int array_hash[200] = {0};
-bool isClosed = false;
-
-void detectAndDisplay(Mat frame,  QSpinBox *rollEdit)
+void AddStudent::detectAndDisplay(Mat frame)
 {
     vector<Rect> faces;
     Mat frame_gray;
@@ -57,18 +54,17 @@ void detectAndDisplay(Mat frame,  QSpinBox *rollEdit)
         cvtColor(crop, gray, COLOR_BGR2GRAY);
         stringstream ssfn;
 
-        folder = ff;
+        folder = this->ff;
+        String name = this->roll;
 
-        String name = rollEdit->text().toStdString();
-
-        ssfn << folder.c_str() << name << filenumber << ".jpg";
+        ssfn << folder.c_str() << name << this->filenumber << ".jpg";
 
         folder = ssfn.str();
         imwrite(folder, res);
 
-        filenumber++;
+        this->filenumber++;
 
-        if(filenumber > 99)
+        if (filenumber > 99)
         {
             filenumber = 10;
             break;
@@ -89,30 +85,26 @@ void detectAndDisplay(Mat frame,  QSpinBox *rollEdit)
     }
 }
 
-void addFace(int i,  QSpinBox *rollEdit, QString classFolder, QString path)
+void AddStudent::addFace(int i, QString vidPath)
 {
-    QString faceFolder = classFolder + "faces/";
-    QString vidpath = classFolder + "video/"+path;
-
-    videoPath = vidpath.toStdString();
-
-    cf = classFolder.toStdString();
-    ff = faceFolder.toStdString();
+    this->videoPath = vidPath.toStdString();
 
     if (i == 1)
     {
         VideoCapture capture(videoPath);
-        if (!capture.isOpened())
-            return;
 
-        if (!face_cascade.load(xmlPath + "haarcascade_frontalface_alt.xml"))
+        if (!capture.isOpened())
+        {
+            return;
+        }
+
+        if (!face_cascade.load(xmlPath))
         {
             cout << "error" << endl;
             return;
         };
 
         int frame_counter = 0;
-
         Mat frame;
 
         while (frame_counter < 80)
@@ -124,13 +116,16 @@ void addFace(int i,  QSpinBox *rollEdit, QString classFolder, QString path)
                 break;
             }
 
-            detectAndDisplay(frame, rollEdit);
+            this->detectAndDisplay(frame);
 
             frame_counter++;
             sleep(0.1);
         }
 
-        eigenFaceTrainer();
+        sleep(1);
+        destroyAllWindows();
+
+        this->eigenFaceTrainer();
         return;
     }
     else
@@ -145,7 +140,7 @@ void addFace(int i,  QSpinBox *rollEdit, QString classFolder, QString path)
 
         Mat frame;
 
-        if (!face_cascade.load(xmlPath + "haarcascade_frontalface_alt.xml"))
+        if (!face_cascade.load(xmlPath))
         {
             cout << "error" << endl;
             return;
@@ -155,12 +150,12 @@ void addFace(int i,  QSpinBox *rollEdit, QString classFolder, QString path)
         {
             capture >> frame;
 
-            detectAndDisplay(frame, rollEdit);
+            this->detectAndDisplay(frame);
             i++;
 
             if (i == 80)
             {
-                cout << "\nFace Added";
+                cout << "\n Face Added";
                 break;
             }
 
@@ -172,131 +167,195 @@ void addFace(int i,  QSpinBox *rollEdit, QString classFolder, QString path)
             }
         }
 
+        this->eigenFaceTrainer();
         return;
     }
-
-    faceFolder = classFolder + "faces";
-    ff = faceFolder.toStdString();
 }
 
 static void dbread(vector<Mat> &images, vector<int> &labels)
 {
     vector<cv::String> fn;
 
-    glob(ff, fn, false);
+    glob(faceFolder, fn, false);
 
     size_t count = fn.size();
 
     for (size_t i = 2; i < count; i++)
     {
         string itsname = "";
+
         char sep = '/';
+
         size_t j = fn[i].rfind(sep, fn[i].length());
+
         if (j != string::npos)
         {
             itsname = (fn[i].substr(j + 1, fn[i].length() - j - 6));
         }
+
         images.push_back(imread(fn[i], 0));
         labels.push_back(atoi(itsname.c_str()));
     }
 }
 
-void eigenFaceTrainer()
+void AddStudent::eigenFaceTrainer()
 {
     vector<Mat> images;
     vector<int> labels;
 
+    faceFolder = this->ff;
+
     dbread(images, labels);
 
-    if(images.size() <= 1)
+    if (images.size() <= 1)
     {
         return;
     }
 
-    cout << "size of the images is " << images.size() << endl;
-    cout << "size of the labels is " << labels.size() << endl;
-    cout << "Training begins...." << endl;
+    cout << " Size of the Images is " << images.size() << endl;
+    cout << " Size of the Labels is " << labels.size() << endl;
+    cout << " Training Begins.... " << endl;
 
     Ptr<EigenFaceRecognizer> model = EigenFaceRecognizer::create();
 
     model->train(images, labels);
+    model->save(this->ef);
 
-    cout << cf;
+    cout << " Training Finished.... " << endl;
 
-    model->save(cf + "eigenface.yml");
-
-    cout << "Training finished...." << endl;
     waitKey(10000);
 }
 
-void mark_attendance(QString file, Home *home, QString cf, QComboBox *comboBox, QSpinBox *batchEdit,int label){
+Student::Student(int label)
+{
+    roll = to_string(label);
+    this->Label = QString::fromStdString(roll);
 
-    ifstream infile;
-    ofstream outfile;
-    string filename=(cf+"/"+comboBox->currentText()+batchEdit->text()+".txt").toStdString();
-    infile.open(filename);
-    outfile.open("temp.txt");
-    string name, batch, branch;
-    int roll_number,attendance;
-    while (infile >> name >> roll_number >> batch >> branch >> attendance)
-    {
-        if (roll_number == label)
-        {
-            attendance++;
-            outfile << name << " " << roll_number << " " << batch << " " << branch << "  " << attendance << "\n";
-            attendance=0;
-        }
-        else
-        {
-            outfile << name << " " << roll_number << " " << batch << " " << branch << "  " << attendance << "\n";
-        }
-    }
-
-    infile.close();
-    outfile.close();
-
-    remove(filename.c_str());
-    rename("temp.txt", filename.c_str());
-
+    present = false;
+    marked = false;
 }
 
-void faceRecognition( QString file, Home *home, QString cf, QComboBox *comboBox, QSpinBox *batchEdit, QString attendanceFile, QDateEdit *dateEdit)
+string Student::Mark(MarkAttendance *M)
 {
-    cout << "start recognizing..." << endl;
+    QFile file(M->SF);
+    QFile attendanceFile(M->AF);
+
+    name = "Unknown";
+
+    if (file.open(QIODevice::ReadOnly | QIODevice::Text))
+    {
+        QStringList studentslist;
+        QTextStream stream(&file);
+
+        while (!stream.atEnd())
+        {
+            studentslist << stream.readLine();
+        }
+
+        int temp;
+        QStringList tokens;
+
+        for (int i = 0; i < studentslist.size(); i++)
+        {
+            tokens = studentslist[i].split(" ");
+
+            int j = 0;
+
+            while (j < tokens.size())
+            {
+                if ((tokens[j] == this->Label) && !marked)
+                {
+                    present = true;
+                    marked = true;
+                    temp = j;
+
+                    break;
+                }
+                else
+                {
+                    j++;
+                }
+            }
+
+            if (present)
+            {
+                name = " ";
+                for (int k = 0; k < temp; k++)
+                {
+                    name += tokens[k] + " ";
+                }
+
+                break;
+            }
+        }
+
+        file.close();
+
+        if (attendanceFile.open(QIODevice::Append | QIODevice::Text))
+        {
+            if (M->array_hash[M->label] == 20)
+            {
+                QTextStream stream(&attendanceFile);
+                stream << name << " " << Label << " Present"
+                       << "\n";
+                marked = true;
+            }
+        }
+
+        attendanceFile.close();
+    }
+
+    return name.toStdString();
+}
+
+MarkAttendance::MarkAttendance(QString classFolder, QComboBox *subBox, QComboBox *comboBox, QSpinBox *batchEdit, QString today)
+{
+    this->cf = classFolder.toStdString();
+    this->ef = (classFolder + "eigenface.yml").toStdString();
+    this->SF = classFolder + comboBox->currentText() + batchEdit->text() + ".txt";
+    this->sf = SF.toStdString();
+    this->AF = classFolder + subBox->currentText() + "/" + today + ".txt";
+    this->af = AF.toStdString();
+}
+
+void MarkAttendance::faceRecognition()
+{
+    cout << " Start Recognizing... " << endl;
+
     Ptr<FaceRecognizer> model = FisherFaceRecognizer::create();
 
-    eigenFile = file.toStdString();
+    model->read(this->ef);
 
-    model->read(eigenFile);
-
+    string window = "Mark Attendance";
     int img_width = 128;
     int img_height = 128;
-    string window = "Capture - face detection";
 
-    if (!face_cascade.load("/home/teja/OpenCV/opencv/data/haarcascades/haarcascade_frontalface_alt.xml"))
+    string text;
+
+    if (!face_cascade.load(xmlPath))
     {
-        cout << " Error loading file" << endl;
+        cout << " Error Loading File" << endl;
         return;
     }
 
     VideoCapture cap(0);
-
 
     if (!cap.isOpened())
     {
         cout << "exit" << endl;
         return;
     }
-    namedWindow(window,1);
+
+    namedWindow(window, 1);
     long count = 0;
 
-    while(1)
+    while (1)
     {
         vector<Rect> faces;
         Mat frame;
         Mat grayScaleFrame;
         Mat original;
-        string temp;
+
         cap >> frame;
         count = count + 1;
 
@@ -308,7 +367,6 @@ void faceRecognition( QString file, Home *home, QString cf, QComboBox *comboBox,
 
             face_cascade.detectMultiScale(grayScaleFrame, faces, 1.1, 3, 0, cv::Size(90, 90));
             string frameset = to_string(count);
-            string faceset = to_string(faces.size());
 
             for (int i = 0; i < faces.size(); i++)
             {
@@ -316,57 +374,60 @@ void faceRecognition( QString file, Home *home, QString cf, QComboBox *comboBox,
                 Mat face = grayScaleFrame(face_i);
 
                 Mat face_resized;
-                cv::resize(face, face_resized, Size(img_width, img_height), 1.0, 1.0, INTER_CUBIC);
+                resize(face, face_resized, Size(img_width, img_height), 1.0, 1.0, INTER_CUBIC);
 
-                int label = -1;
+                label = -1;
                 double confidence = 0;
 
                 model->predict(face_resized, label, confidence);
-                label = label/10;
+                label = label / 10;
 
                 array_hash[label]++;
 
-
-                if (array_hash[label] <= 1)
+                if (array_hash[label] == 20)
                 {
-                    home->present=true;
-                    mark_attendance(file,home,cf, comboBox, batchEdit,label);
-
-
+                    Student st(label);
+                    text = st.Mark(this);
+                    marked = true;
                 }
-             cout << " confidence " << confidence << " Label: " << label << endl;
 
+                cout << " Confidence " << confidence << " Label : " << label << endl;
 
-                // drawing green rectagle in recognize face
                 rectangle(original, face_i, CV_RGB(0, 0, 255), 1);
-                //string text = Pname;
 
                 int pos_x = max(face_i.tl().x - 10, 0);
                 int pos_y = max(face_i.tl().y - 10, 0);
-                string text=to_string(label);
-                // name the person who is in the image
-                putText(original, text, Point(pos_x, pos_y), FONT_HERSHEY_COMPLEX_SMALL, 1.0, CV_RGB(0, 255, 0), 1.0);
+
+                putText(original, text , Point(pos_x, pos_y), FONT_HERSHEY_COMPLEX_SMALL, 1.0, CV_RGB(0, 255, 0), 1.0);
             }
 
-            putText(original, "Frames: " + frameset, Point(30, 60), FONT_HERSHEY_COMPLEX_SMALL, 1.0, CV_RGB(0, 255, 0), 1.0);
-            putText(original, "No. of Persons detected: " + to_string(faces.size()), Point(30, 90), FONT_HERSHEY_COMPLEX_SMALL, 1.0, CV_RGB(0, 255, 0), 1.0);
+            putText(original, "Frames : " + frameset, Point(30, 60), FONT_HERSHEY_COMPLEX_SMALL, 1.0, CV_RGB(0, 255, 0), 1.0);
+            putText(original, "Persons detected: " + to_string(faces.size()), Point(30, 90), FONT_HERSHEY_COMPLEX_SMALL, 1.0, CV_RGB(0, 255, 0), 1.0);
 
             cv::imshow(window, original);
         }
         if (waitKey(30) == 27)
         {
-          // destroyWindow("Web Cam" );
+            break;
+        }
+
+        if (marked)
+        {
+            sleep(3);
+            destroyWindow(window);
             break;
         }
     }
+
+    return;
 }
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
 
-    Home w;
-    w.show();
+    Home home;
+    home.show();
 
     return a.exec();
 }
